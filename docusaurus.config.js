3e7d0cb5519888dc5d8121682e5680d522bf8f9a
skgandikota <sai.gandikota@marks-and-spencer.com>
2022-01-04 07:22:46 +0530

    2022-01-04-07-20-19

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Dharma Sandehalu',
+  tagline: 'Devotional Doubts to our brother & sisters',
+  url: 'https://dharmasandehalu.co.in',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'IgniteResearch', // Usually your GitHub org/user name.
+  projectName: 'Dharma-Sandehalu', // Usually your repo name.
 
   presets: [
     [
